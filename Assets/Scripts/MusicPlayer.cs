d42d553c22bee301cd3664588c04ece33588bf03
Sean Ryan <sryan@evercrack.com>
2017-07-09 17:16:22 -0400

    Add sound manager and background music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	private static MusicPlayer instance = null;
+
+	public static MusicPlayer Instance { get { return instance; } }
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);	
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+
+}
